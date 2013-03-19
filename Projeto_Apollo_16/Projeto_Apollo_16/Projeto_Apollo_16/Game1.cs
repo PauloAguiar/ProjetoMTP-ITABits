3ae0920b26f5c0e5470d6c7fd3c50a61c505933b
Felipe Viana (Flash) <felipe.140492@gmail.com>
2013-03-19 03:53:12 -0300

    movimento da nave
    testem aí pra ver como que tá, dem palipites

--- Projeto_Apollo_16/Projeto_Apollo_16/Projeto_Apollo_16/Game1.cs
+++ Projeto_Apollo_16/Projeto_Apollo_16/Projeto_Apollo_16/Game1.cs
@@ -11,11 +11,10 @@ using Microsoft.Xna.Framework.Media;
 
 namespace Projeto_Apollo_16
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
+        Nave nave;
+
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
@@ -25,65 +24,43 @@ namespace Projeto_Apollo_16
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            Texture2D naveTextureNormal = Content.Load<Texture2D>("Sprites\\Nave\\nave");
+            Texture2D naveTextureRight = Content.Load<Texture2D>("Sprites\\Nave\\nave-right");
 
-            // TODO: use this.Content to load your game content here
+
+            nave = new Nave(new Vector2(200),naveTextureNormal, naveTextureRight);
+            
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 this.Exit();
 
-            // TODO: Add your update logic here
+            nave.Update(gameTime);
 
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            nave.Draw(spriteBatch);
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ Projeto_Apollo_16/Projeto_Apollo_16/Projeto_Apollo_16/Nave.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Projeto_Apollo_16
+{
+    class Nave
+    {
+        /////////////
+        //fields/////
+        /////////////
+        //private float Angulo;
+        private Vector2 position;
+        private Vector2 velocity;
+        private Texture2D textureNormal;
+        private Texture2D textureRight;
+        private enum Direcoes
+        {
+            normal, right, left 
+        };
+        private Direcoes direcao = Direcoes.normal;
+        
+        //////////
+        //ctor////
+        /////////
+        public Nave(Vector2 posicao, Texture2D texturaNormal, Texture2D texturaDireita)
+        {
+            position = posicao;
+            textureNormal = texturaNormal;
+            textureRight = texturaDireita;
+            direcao = Direcoes.normal;
+        }
+
+        ////////////
+        //methods///
+        ///////////
+        public Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+        public Vector2 Velocity
+        {
+            get { return velocity; }
+            set { velocity = value; }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            velocity = new Vector2(0);
+
+            if(Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                velocity+=new Vector2(0,-1);
+            }
+            if(Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                velocity+=new Vector2(0,1);
+            }
+            if(Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                velocity+=new Vector2(-1,0);
+            }
+            if(Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                velocity+=new Vector2(1,0);
+            }
+
+            if (velocity.X > 0)
+            {
+                direcao = Direcoes.right;
+            }
+            else if (velocity.X < 0)
+            {
+                direcao = Direcoes.left;
+            }
+            else
+            {
+                direcao = Direcoes.normal;
+            }
+
+            position += 2*velocity;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (direcao == Direcoes.normal)
+            {
+                spriteBatch.Draw(textureNormal, position, Color.White);
+            }
+            else if (direcao == Direcoes.right)
+            {
+                spriteBatch.Draw(textureRight, position, Color.White);
+            }
+            else
+            {
+                spriteBatch.Draw(textureRight, position, null, Color.White, 0, new Vector2(0), 1, SpriteEffects.FlipHorizontally, 0);
+            }
+        }
+
+
+    }
+}
