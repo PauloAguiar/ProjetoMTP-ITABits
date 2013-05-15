a6a0bc73391b7fd93f9a7a6dfda37e3e5b3732b0
Natan Lima Viana <natanvianat16@gmail.com>
2013-05-14 23:24:51 -0300

    Criei a classe item

---
+++ Projeto_Apollo_16/Projeto_Apollo_16/Projeto_Apollo_16/ItemClasses/BaseItem.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Projeto_Apollo_16.ItemClasses
+{
+    //public enum Wings { One, Two }
+    //public enum WeaponLocation { Wings, Tail ,   }
+    public abstract class BaseItem
+    {
+        #region Field Region
+        
+        protected List<Type> allowableClasses = new List<Type>();
+        
+        string name;
+        string type;
+        bool equipped;
+         
+        #endregion
+         
+        #region Property Region
+        
+        public List<Type> AllowableClasses
+        {
+            get { return allowableClasses; }
+            protected set { allowableClasses = value; }
+        }
+        public string Type
+        {
+            get { return type; }
+            protected set { type = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            protected set { name = value; }
+        }
+        public bool IsEquiped
+        {
+            get { return equipped; }
+            protected set { equipped = value; }
+        }
+        #endregion
+
+        #region Constructor Region
+        
+        public BaseItem(string name, string type, params Type[]
+        allowableClasses)
+        {
+            foreach (Type t in allowableClasses)
+                AllowableClasses.Add(t);
+         Name = name;
+         Type = type;
+         IsEquiped = false;
+        }
+         
+        #endregion
+         
+        #region Abstract Method Region
+        
+        public abstract object Clone();
+        
+        public virtual bool CanEquip(Type characterType)
+        {
+            return allowableClasses.Contains(characterType);
+        }
+        public override string ToString()
+        {
+            string itemString = "";
+            itemString += Name + ", ";
+            itemString += Type + ", ";
+            return itemString;
+        }
+        #endregion
+         }
+        }
+    }
+}
