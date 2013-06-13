7b2328a38a14f0a34dbd1a22824813a793e872e1
Felipe Viana (Flash) <felipe.140492@gmail.com>
2013-06-13 13:11:20 -0300

    Conflitos

---
+++ Projeto_Apollo_16/Projeto_Apollo_16/Projeto_Apollo_16/Actors/ItemClasses/ItemClass.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace Projeto_Apollo_16
+{
+    public abstract class ItemClass : ActorClass
+    {
+        protected string name;
+        public double timeLiving;
+        public double ttl { get; protected set; }
+        protected bool taken;
+        protected PlayerClass player;
+        public bool IsUsing { get; protected set; }
+
+        public ItemClass(PlayerClass player, Vector2 position, ContentManager content)
+        {
+            globalPosition = position;
+            timeLiving = 0;
+            taken = false;
+            IsUsing = true;
+            this.player = player;
+            this.LoadFont(content);
+            this.LoadTexture(content);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (CollisionManager.CircularCollision(this, player))
+            {
+                taken = true;
+            }
+            else if (taken)
+            {
+                taken = false;
+            }
+        }
+
+        public override void LoadTexture(ContentManager content)
+        {
+            texture = content.Load<Texture2D>(@"Sprites\items\item");
+        }
+
+        public override void LoadFont(ContentManager content)
+        {
+            spriteFont = content.Load<SpriteFont>(@"Fonts\ActorInfo");
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            if (!taken)
+            {
+                spriteBatch.Draw(texture, globalPosition, Color.White);
+            }
+        }
+
+    }
+}
---
+++ Projeto_Apollo_16/Projeto_Apollo_16/Projeto_Apollo_16/Actors/ItemClasses/Shield.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace Projeto_Apollo_16
+{
+    public class Shield : ItemClass
+    {
+        private const double lifeTime = 5000;
+        private double timeLiving;
+        
+        public Shield(int health, PlayerClass player, Vector2 position, ContentManager content) : base (player, position, content)
+        {
+            //this.name = name;
+            name = "shield";
+            ttl = 10000;
+            timeLiving = 0;
+            IsUsing = true;
+
+        }
+
+        public override void  Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            timeLiving += gameTime.ElapsedGameTime.TotalMilliseconds;
+            if(timeLiving >= lifeTime)
+            {
+                IsUsing = false;
+                //destroy
+            }
+            
+        }
+
+        public override void  LoadTexture(ContentManager content)
+        {
+            texture = content.Load<Texture2D>(@"Sprites\items\shield");
+        }
+    }
+}
