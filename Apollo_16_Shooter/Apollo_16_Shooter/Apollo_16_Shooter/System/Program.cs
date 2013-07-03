58a4b1ea57249704ea070ee80f51583a8420e929
Paulo Henrique Aguiar <phaguiardm@gmail.com>
2013-07-03 00:25:38 -0300

    Criado Shooter Project

---
+++ Apollo_16_Shooter/Apollo_16_Shooter/Apollo_16_Shooter/System/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Threading;
+
+namespace Apollo_16_Shooter
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (SystemClass game = new SystemClass())
+            {
+                game.Run();
+            }
+
+        }
+    }
+#endif
+}
+
