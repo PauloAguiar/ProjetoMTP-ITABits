f7b04178d62d946451012b336a2b6f80c391603a
Felipe Viana (Flash) <felipe.140492@gmail.com>
2013-07-03 05:24:09 -0300

    Ajeitado o life

--- Apollo_16_Piloto/Apollo_16_Piloto/Apollo_16_Piloto/Pilot/PilotClass.cs
+++ Apollo_16_Piloto/Apollo_16_Piloto/Apollo_16_Piloto/Pilot/PilotClass.cs
@@ -79,19 +79,19 @@ namespace Apollo_16_Piloto
             if (fuel >= 50)
                 fuelbar_color = Color.Lerp(Color.Green, Color.Yellow, 1.0f - fuel/100.0f);
             else
-                fuelbar_color = Color.Lerp(Color.Yellow, Color.Red,1.0f - fuel/100.0f);
+                fuelbar_color = Color.Lerp(Color.Yellow, Color.Red,1.0f - 2*fuel/100.0f);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
             //Rectangle rlife = new Rectangle(0,0,life*lifeBar.Width/150,lifeBar.Height);
             //Rectangle rfuel = new Rectangle((int)(fuelBar.Width/2.0f + 500.0f),500 - (int)fuel/2, fuelBar.Width, (int)fuel*fuelBar.Height/100);
-            Rectangle rfuel = new Rectangle(0, (int)((fuelBar.Height) * (fuel) / 100.0f), fuelBar.Width, (int)((fuelBar.Height) * (fuel) / 100.0f));
+            Rectangle rfuel = new Rectangle(0, (int)((fuelBar.Height) * (100.0f-fuel) / 100.0f), fuelBar.Width, (int)((fuelBar.Height) * (fuel) / 100.0f));
             
             
             spriteBatch.DrawString(spriteFont, "Th:" + throttle.ToString() + " Spd:" + speed.ToString() + " ang:" + angle.ToString() + " vel:" + velocity.ToString() , new Vector2(0, 100), Color.Red);
             //spriteBatch.Draw(lifeBar,new Vector2(Globals.SCREEN_RESOLUTION_WIDTH-lifebar.Width,0.0f),rlife,lifebar_color);
-            spriteBatch.Draw(fuelBar, new Vector2(0.0f, 0.0f),rfuel, fuelbar_color);
+            spriteBatch.Draw(fuelBar, new Vector2(0.0f, (fuelBar.Height) * (100.0f - fuel) / 100.0f), rfuel, fuelbar_color);
             //spriteBatch.Draw(speedometer, new Vector2(100.0f, 100.0f),null, Color.White);
             //spriteBatch.Draw(speedometer, new Vector2(400.0f, 100.0f), null, Color.White);
             //spriteBatch.Draw(arrow, new Vector2(100.0f+speedometer.Width/2, 100.0f+speedometer.Height/2), null, Color.White, (float)Math.Abs(speed), new Vector2(arrow.Width / 2, arrow.Height), 1.0f, new SpriteEffects(), 0.0f);
